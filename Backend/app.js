e281b2ed10ca915f4a17d9b996adecac7182fa8e
Ayushi-Pattanaik-28 <ayushipattanaik94458@gmail.com>
2024-12-08 21:42:08 +0530

    changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+//get
+app.get("/", (req,res) => {
+    res.send("Hello");
+})
+
 //creating Port
 app.listen(1000, () => {
     console.log("Server Started");
