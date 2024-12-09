a75528a43e0fd21ccd37597dfb861c93aea6af40
Ayushi-Pattanaik-28 <ayushipattanaik94458@gmail.com>
2024-12-09 16:30:57 +0530

    change

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+
 require("dotenv").config();
+require("./conn/conn");
 
 //creating Port
 app.listen(process.env.PORT, () => {
     console.log(`Server Started at port ${process.env.PORT}`);
 });
+
 
