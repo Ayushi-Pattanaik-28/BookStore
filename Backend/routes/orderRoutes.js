dbe9ef5bc79b8dc07c2f352c53a1c1b3a774e570
Ayushi-Pattanaik-28 <ayushipattanaik94458@gmail.com>
2024-12-20 13:35:23 +0530

    add

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -3,4 +3,15 @@ const { authenticateToken } = require("./userAuth");
 const Book = require("../models/book");
 const Order = require("../models/order");
 
+//place order
+router.post("/place-order", authenticateToken, async (req, res) => {
+    try{
+        const { id } = req.headers;
+        const { order } = req.body;
+        for (const orderData of order) {
+            const 
+        }
+    }
+})
+
 module.exports = router;
